8d87346a4b0b24efc9e0f1e4cb88547d1cc39b9d
hiHjy <huangjiyaun123@gmail.com>
2025-07-06 00:35:48 +0800

    fix bug

--- Linux_system_programming/1_open/main.c
+++ Linux_system_programming/1_open/main.c
@@ -19,12 +19,14 @@
  * @param mode 打开一个文件若文件不存在则创建它的初始权限0xxx(八进制) 0644
  *              文件权限 = mode & ~umask
  * @retval 成功 fd 失败 -1 set errno
- * @note 需要头文件<sys/types.h>  <sys/stat.h> <fcntl.h> ，其中<sys/types.h>  <sys/stat.h>
+ * @note 1，需要头文件<sys/types.h>  <sys/stat.h> <fcntl.h> ，其中<sys/types.h>  <sys/stat.h>
  *       包括在<unistd.h>中
+ *      2，O_TRUNC 必须配 O_WRONLY 或 O_RDWR
+ *    
  */
 int main(int argc, char const *argv[])
 {
-    int fd1 = open("./test.txt", O_RDONLY | O_CREAT | O_TRUNC, 0644);
+    int fd1 = open("./test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
     //打开不存在的文件
     int fd2 = open("./test1.txt", O_RDONLY);
     //fd = -1 set errno use strerr(errno) to check msg
